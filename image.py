092f5930cee7175b01af6bfdf7d65340bd53506f
ben5Kuda <kmkuda@gmail.com>
2020-05-20 20:57:34 +0200

    add requirements

--- image.py
+++ image.py
@@ -1,25 +1,20 @@
-import base64
-from typing import BinaryIO
-
+import flask
 from flask import Flask, request, Response, jsonify
 
 app = Flask(__name__)
 
+print(flask.__version__)
 
 @app.route('/api/myimages', methods=['POST'])
 def myimages():
     req = request
 
+
+
     if req == None:
         print('The request is null')
     else:
         print(req.data)
-
-    # image_decode = base64.decodebytes(req.data)
-    # print(type(image_decode))
-    # with open('me.png', 'wb') as dimage:
-    #     dimage.write(image_decode)
-
     response = {"image": req.data}
     # response_pickled = jsonpickle.encode(response)
     message = {
